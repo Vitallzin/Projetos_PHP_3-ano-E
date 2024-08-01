ece10c1a1d53ad62661474863f8caee223ce35a1
Vitallzin <163913876+Vitallzin@users.noreply.github.com>
2024-08-01 14:55:50 -0300

    Introdução de Array

---
+++ Aula6/aula6.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 6 bro</title>
+    <link rel="stylesheet" href="style.css">
+ 
+</head>
+<body>
+    <?php
+    echo"<h1>Arrays/Vetores</h1>";
+    $carros = array('Ferrari','Lamborghini','BWM','Bugatti');
+    echo $carros[1], "<br>";
+    echo $carros[3], "<br>";
+     $carros[4] = "Clio";
+     $carros[11] = "Versa";
+     $carros[ ] = "Sandro";
+     echo $carros[4], "<br>";
+     echo $carros[5], "<br>";
+     echo $carros[11], "<br>";
+     echo $carros[12], "<br>";
+
+
+
+
+
+
+
+
+
+    ?>
+
+    
+</body>
+</html>
