66b1ccde058a7f8790805cb8a690090463018232
Vitallzin <163913876+Vitallzin@users.noreply.github.com>
2024-08-05 18:28:10 -0300

    Add files via upload

---
+++ Aula07/Recebe-aula07.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    
+    echo"Olá ",$_GET["name"]," ",$_GET["Sobrenome"]," Seja Bem-vindo meu nobre!";
+
+    ?>
+    
+</body>
+</html>
