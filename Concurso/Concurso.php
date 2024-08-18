421572cbb19b3c8faa9f4528f8022ad0120d3257
Vitallzin <163913876+Vitallzin@users.noreply.github.com>
2024-08-18 18:30:35 -0300

    Concurso.php

--- Concurso/Concurso.php
+++ Concurso/Concurso.php
@@ -11,9 +11,20 @@
 
 <p><strong>Nome completo: </strong><?php echo $_POST["nomecompleto"] ?></p>
 <p><strong>Data de Nacimento: </stong><?php echo $_POST["DataNasc"] ?><p>
+<p><strong>Nacionalidade: </stong><?php echo $_POST["nacionalidade"] ?><p>
+<p><strong>Sexo: </stong><?php echo $_POST["Sexo"] ?><p>
+<p><strong>Número do RG: </stong><?php echo $_POST["RG"] ?><p>
 <p><strong>CPF: </strong><?php echo $_POST["cpf"] ?></p>
+<p><strong>CEP: </strong><?php echo $_POST["cep"] ?></p>
+<p><strong>logradouro: </strong><?php echo $_POST["logradouro"] ?></p>
+<p><strong>Número: </strong><?php echo $_POST["numero"] ?></p>
+<p><strong>Complemento: </strong><?php echo $_POST["complemento"] ?></p>
+<p><strong>Bairro: </strong><?php echo $_POST["bairro"] ?></p>
+<p><strong>Cidade: </strong><?php echo $_POST["cidade"] ?></p>
+<p><strong>Estado: </strong><?php echo $_POST["estado"] ?></p>
 <p><strong>Telefone: </strong><?php echo $_POST["telefone"] ?></p>
 <p><strong>Email: </strong><?php echo $_POST["email"] ?></p>
+
 
 
     
