a4ba6612757bd12e96ef8573e713120dcdd78afd
Vitallzin <163913876+Vitallzin@users.noreply.github.com>
2024-08-19 17:41:02 -0300

    Concurso.php

--- Concurso/Concurso.php
+++ Concurso/Concurso.php
@@ -10,10 +10,10 @@
 <h1>Página de Confirmação</h1>
 
 <p><strong>Nome completo: </strong><?php echo $_POST["nomecompleto"] ?></p>
-<p><strong>Data de Nacimento: </stong><?php echo $_POST["DataNasc"] ?><p>
-<p><strong>Nacionalidade: </stong><?php echo $_POST["nacionalidade"] ?><p>
-<p><strong>Sexo: </stong><?php echo $_POST["Sexo"] ?><p>
-<p><strong>Número do RG: </stong><?php echo $_POST["RG"] ?><p>
+<p><strong>Data de Nacimento: </strong><?php echo $_POST["DataNasc"] ?><p>
+<p><strong>Nacionalidade: </strong><?php echo $_POST["nacionalidade"] ?><p>
+<p><strong>Sexo: </strong><?php echo $_POST["Sexo"] ?><p>
+<p><strong>Número do RG: </strong><?php echo $_POST["RG"] ?><p>
 <p><strong>CPF: </strong><?php echo $_POST["cpf"] ?></p>
 <p><strong>CEP: </strong><?php echo $_POST["cep"] ?></p>
 <p><strong>logradouro: </strong><?php echo $_POST["logradouro"] ?></p>
