45a879b55a15f0e79a091fd7f744044fc1b56f74
Vitallzin <163913876+Vitallzin@users.noreply.github.com>
2024-08-23 15:33:44 -0300

    Add files via upload

---
+++ Aula8/recebe8.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Olá, ", $_POST["nome"], " ", $_POST["sobrenome"],"!", " Seja bem-vindo cachorrao"
+
+    ?>
+    
+</body>
+</html>
